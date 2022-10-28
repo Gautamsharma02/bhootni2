be762d6fcd9561d3ce4728c4c4a836555c57d17c
Gautamsharma02 <gautamji94624@gmail.com>
2022-10-29 01:05:47 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,49 @@
+// let image1 = document.getElementById("image1");
+
+// setTimeout(function() {
+//     image1.classList.add('none');
+//     image2.classList.add('block');
+// }, 8000);
+
+$('#btn1').click(function() {
+    $('#section1').addClass('none');
+});
+$('#btn1').click(function() {
+    $('#section2').addClass('block');
+});
+$('#btn2').click(function() {
+    $('#section2').removeClass('block');
+});
+$('#btn2').click(function() {
+    $('#section2').addClass('none');
+});
+$('#btn2').click(function() {
+    $('#section3').addClass('block');
+});
+$('#btn3').click(function() {
+    $('#section3').removeClass('block');
+});
+$('#btn3').click(function() {
+    $('#section3').addClass('none');
+});
+$('#btn3').click(function() {
+    $('#section4').addClass('block');
+});
+$('#btn4').click(function() {
+    $('#section4').removeClass('block');
+});
+$('#btn4').click(function() {
+    $('#section4').addClass('none');
+});
+$('#btn4').click(function() {
+    $('#section5').addClass('block');
+});
+$('#btn5').click(function() {
+    $('#section5').removeClass('block');
+});
+$('#btn5').click(function() {
+    $('#section5').addClass('none');
+});
+$('#btn5').click(function() {
+    $('#section6').addClass('block');
+});
